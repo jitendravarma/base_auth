5d0500d36a940026e2bbe39b307eb02b1d040dc5
jitendra <jitendra@fafadiatech.com>
2020-01-29 14:44:07 +0530

    added dashboard.html

--- media_manager/core/views.py
+++ media_manager/core/views.py
@@ -113,7 +113,7 @@ class IndexView(LoginRequiredMixin, TemplateView):
     Home view for user after redirection
     """
 
-    template_name = 'index.html'
+    template_name = 'frontend/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
