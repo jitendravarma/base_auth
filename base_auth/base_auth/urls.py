a1602420d6b24b97f1feab4f98293fa27d8150e1
WitchKing's <witchkings@WitchKingss-MacBook-Pro.local>
2020-04-23 23:32:37 +0530

    rename app and migrated project

--- base_auth/base_auth/urls.py
+++ base_auth/base_auth/urls.py
@@ -1,4 +1,4 @@
-"""media_manager URL Configuration
+"""base_auth URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
